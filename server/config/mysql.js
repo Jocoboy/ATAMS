9c25ccb5fc3e08d244f78da5a58888aaa6c5ae5d
Jocoboy <50202665+Jocoboy@users.noreply.github.com>
2020-06-21 23:54:32 +0800

    Connect server to mysql，but failed.

---
+++ server/config/mysql.js
@@ -0,0 +1,9 @@
+var mysql = {
+    localhost: '127.0.0.1',
+    user: 'root',
+    password: 'Jocoboy10150315021525',
+    database: 'MySQL80',
+    port: 3306
+};
+
+module.exports = mysql;
--- website/src/router/index.js
+++ website/src/router/index.js
@@ -3,17 +3,40 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+
+// import Layout from '@/layout'
+// import { component } from 'vue/types/umd'
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
 export const constantRoutes = [
 
   {
     path:'/',
-    redirect: '/login'
+    redirect: '/login',
   },
 
   {
     path: '/login',
     component:()=> import('../views/login/index')
   },
+
+  // {
+  //   path: '/dashboard',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       component: ()=> import('@views/dashboard/index'),
+  //       name: 'Dashboard',
+  //       meta: { title : 'Dashboard' , icon: 'dashboard', affix: true}
+  //     }
+     
+  //   ]
+  // },
+
   {
     path: '/404',
     component:()=> import('../views/error/404'),
@@ -23,14 +46,37 @@ export const constantRoutes = [
     path: '/401',
     component:()=> import('../views/error/401'),
     hidden: true
-  }
+  },
+
+  { path: '*', redirect: '/404', hidden: true}
+]
+
+/**
+ * asyncRoutes
+ * the routes that need to be dynamically loaded based on user roles
+ */
+export const asyncRoutes = [
+
+
 ]
 
 const createRouter = () => new Router({
+  // scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
 
 const router = createRouter()
 
+// export function resetRouter(){
+//   const newRouter = createRouter()
+//   router.matcher = newRouter.matcher
+// }
+
+// router.beforeEach((to,from,next)=>{
+//   if(to.meta.title){
+//     document.title = to.meta.title
+//   }
+//   next()
+// })
 
 export default router
