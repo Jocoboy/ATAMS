75ea2ffee83efdc63af138c12eee45dc12750d77
Jocoboy <50202665+Jocoboy@users.noreply.github.com>
2020-06-22 23:35:01 +0800

    Remove login page from Vue to Express.

--- server/app.js
+++ server/app.js
@@ -3,22 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// 创建服务器
 var app = express();
+app.listen(8080);
 
-// view engine setup
+// 设置views文件夹为存放视图资源的目录
 app.set('views', path.join(__dirname, 'views'));
+// 设置视图模板引擎为ejs
 app.set('view engine', 'ejs');
 
+// 加载favicon中间件
+app.use(favicon(path.join(__dirname,'/public/favicon.ico')));
+// 加载日志中间件
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// Express内置中间件，用于解析json，并基于body-parser(在Express v4.16.0及之后版本中提供)
+app.use(express.json());  // app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false })); // app.use(bodyParser.urlencoded({ extended: false }))：
+// 加载解析cookie的中间件
 app.use(cookieParser());
+// 设置public文件夹为存放静态资源的目录
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 路由控制
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
